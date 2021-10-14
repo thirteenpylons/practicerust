6417d0e04d6d9518259f60e67555198b09086935
thirteenpylons <thirteenpylons@gmail.com>
2021-10-14 19:33:58 -0400

    rm spoiler

--- guessing_game/target/debug/guessing_game.d
+++ guessing_game/target/debug/guessing_game.d
@@ -1,1 +1,1 @@
-/home/booch/Documents/Git/guessing_game/target/debug/guessing_game: /home/booch/Documents/Git/guessing_game/src/main.rs
+/home/booch/Documents/Git/practicerust/guessing_game/target/debug/guessing_game: /home/booch/Documents/Git/practicerust/guessing_game/src/main.rs
