5eadc23fbb9f30066c9bc5d094357c51b508467f
thirteenpylons <thirteenpylons@gmail.com>
2021-10-14 20:35:58 -0400

    adding to mgrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,30 @@
 use std::env;
+use std::fs;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let (query, filename) = parse_config(&args);
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename, String,
+}
+
+
+fn parse_config(args: &[String]) -> Config {
+    let query = &args[1].clone();
+    let filename = &args[2].clone();
+
+    Config { query, filename }
 }
