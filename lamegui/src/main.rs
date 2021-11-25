a153b325b57aaa4ec55971ba2cb0577c0d72cfc7
thirteenpylons <thirteenpylons@gmail.com>
2021-11-24 19:54:46 -0500

    no idea

--- lamegui/src/main.rs
+++ lamegui/src/main.rs
@@ -1,3 +1,15 @@
+use orbtk::prelude::*
+
+
 fn main() {
-    println!("Hello, world!");
+    Application::new()
+        .window(|ctx| {
+            Window::new()
+                .title("Lamegui - min")
+                .position((100.0, 100.0))
+                .size(420.0, 730.0)
+                .child(TextBlock::new().text("Lame").build(ctx))
+                build(ctx)
+        })
+        .run();
 }
