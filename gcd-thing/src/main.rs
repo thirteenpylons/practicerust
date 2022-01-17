39e54a30fb49632c7b31659255761b5055bdb631
thirteenpylons <thirteenpylons@gmail.com>
2022-01-17 08:39:16 -0500

    finished

--- gcd-thing/src/main.rs
+++ gcd-thing/src/main.rs
@@ -2,6 +2,29 @@ use actix_web::{web, App, HttpResponse, HttpServer};
 use serde::Deserialize;
 
 
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17,
+                   3 * 7 * 11 * 13 * 19),
+                3 * 11);
+}
+
+
 #[derive(Deserialize)]
 struct GcdParameters {
     n: u64,
@@ -18,7 +41,7 @@ fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
 
     let response =
         format!("The greatest common divisor of the numbers {} and {} \
-                is <b>{}</b>\n"),
+                is <b>{}</b>\n",
                 form.n, form.m, gcd(form.n, form.m));
 
     HttpResponse::Ok()
@@ -30,6 +53,7 @@ fn main() {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_gcd))
     });
 
     println!("Serving on http://localhost:3000...");
