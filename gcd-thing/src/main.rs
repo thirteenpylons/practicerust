cc45e7557fdfac2b2cb942ec784f9f6332461997
thirteenpylons <thirteenpylons@gmail.com>
2022-01-16 19:45:28 -0500

    redo

--- gcd-thing/src/main.rs
+++ gcd-thing/src/main.rs
@@ -1,24 +1,56 @@
-use std::str::FromStr;
-use std::env;
+use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
+
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
+}
+
+
+fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with zero is boring.");
+    }
+
+    let response =
+        format!("The greatest common divisor of the numbers {} and {} \
+                is <b>{}</b>\n"),
+                form.n, form.m, gcd(form.n, form.m));
+
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response)
+}
 
 fn main() {
-    let mut numbers = Vec::new();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(get_index))
+    });
 
-    for arg in env::args().skip(1) {
-        numbers.push(u64::from_str(&arg)
-                    .expect("error parsing argument"));
-    }
+    println!("Serving on http://localhost:3000...");
+    server
+        .bind("127.0.0.1:3000").expect("error binding server to address")
+        .run().expect("error running server");
+}
 
-    if numbers.len() == 0 {
-        eprintln!("Usage: gcd NUMBER ...");
-        std::process::exit(1);
-    }
 
-    let mut d = numbers[0];
-    for m in &numbers[1..] {
-        d = gcd(d, *m);
-    }
+fn get_index() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(
+            r#"
+                <title>GCD Calculator</title>
+                <form action="/gcd" method="post">
+                <input type="text" name="n"/>
+                <input type="text" name="m"/>
+                <button type="submit">Compute GCD</button>
+                </form>
 
-    println!("The greatest common divisor of {:?} is {}",
-            numbers, d);
+            "#,
+        )
 }
