d8c7ea938563ec88ce6760c3e250955ad910159f
thirteenpylons <thirteenpylons@gmail.com>
2022-01-18 09:03:09 -0500

    this

--- practice/src/main.rs
+++ practice/src/main.rs
@@ -1,18 +1,35 @@
-use std::io;
+
+
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+// This is a really bad adding function, its purpose is to fail in this
+// example.
+#[allow(dead_code)]
+fn bad_add(a: i32, b: i32) -> i32 {
+    a - b
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scopre.
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
+
+    #[test]
+    fn test_bad_add() {
+        // This assert would fire and test will fail
+        // Please note, that private functions can be tested too!
+        assert_eq!(bad_add(1, 2), 3);
+    }
+}
+
 
 fn main() {
-
-    let mut m = "this";
-    let x = *m;
-    println!("{}", &String x);
 }
 
-// ask for name
-fn set_name() -> io::Result<()> {
-    // gets the name
-    let mut input = String::new();
-
-    io::stdin().read_line(&mut input)?;
-    
-    Ok(())
-}
