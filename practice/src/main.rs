fe4819623d37d46635769517daaed472e0cfedf1
thirteenpylons <thirteenpylons@gmail.com>
2022-01-20 14:57:33 -0500

    todo

--- practice/src/main.rs
+++ practice/src/main.rs
@@ -1,11 +1,22 @@
 use std::io;
 
 fn main() {
-    let mut n = set_name();
+    let v: Vec<f64> = vec![0.0, 0.707, 1.0, 0.707];
+    let a: [f64; 4] =     [0.0, 0.707, 1.0, 0.707];
 
-    println!("{}", n.len());
+    let sv: &[f64] = &v;
+    let sa: &[f64] = &a;
 
+    print(sv);
+    print(sa);
 }
+
+fn print(n: &[f64]) {
+    for elt in n {
+        println!("{}", elt);
+    }
+}
+
 
 // ask for name
 fn set_name() -> io::Result<()> {
